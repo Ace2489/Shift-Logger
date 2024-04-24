025861adaf2ebf38fae4fe0d04f3b4ff0f8f3546
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2024-04-24 23:47:45 +0100

    Created the UI project and set up the barebones class

---
+++ UI/Program.cs
@@ -0,0 +1,3 @@
+﻿using UI;
+UIApp ui = new("https://localhost:7011/api/shifts");
+await ui.Run();
---
+++ UI/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+using InputHelpers;
+
+namespace UI;
+
+public class UIApp
+{
+    readonly GetTypesFromTerminal getTypes;
+    readonly HttpClient client;
+    public UIApp(string url)
+    {
+        getTypes = new();
+        client = new()
+        {
+            BaseAddress = new Uri(url)
+        };
+        client.DefaultRequestHeaders.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task Run()
+    {
+        HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+        Console.WriteLine(response.Content.);
+        Console.WriteLine("Gotten");
+
+    }
+}
