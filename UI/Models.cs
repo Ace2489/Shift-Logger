a469154b96c7c68a4372c31827968f1eb83598f7
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2024-04-25 14:28:47 +0100

    Built out the UI to allow users view all existing shifts

---
+++ UI/Models.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UI;
+
+public record class ShiftRecord
+(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("startTime")] DateTime StartTime,
+    [property: JsonPropertyName("endTime")] DateTime EndTime,
+    [property: JsonPropertyName("duration")] string Duration
+
+);
+
+public record class AddShift
+{
+    public DateTime StartTime;
+    public DateTime EndTime;
+}
+
--- UI/UI.cs
+++ UI/UI.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Text.Json;
 using InputHelpers;
 
 namespace UI;
@@ -7,6 +9,7 @@ public class UIApp
 {
     readonly GetTypesFromTerminal getTypes;
     readonly HttpClient client;
+
     public UIApp(string url)
     {
         getTypes = new();
@@ -20,9 +23,138 @@ public class UIApp
 
     public async Task Run()
     {
-        HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
-        Console.WriteLine(response.Content.);
-        Console.WriteLine("Gotten");
 
+        //Creating a new entry
+        //Ask for start time
+        //Ask for end time
+        /*Send request to create
+
+        //Get all the shifts
+        */
+        while (true)
+        {
+            Console.Clear();
+            string operation = MainMenu().ToLower().Trim();
+            if (operation.Equals("e"))
+            {
+                break;
+            }
+            else if (operation.Equals("c"))
+            {
+                DateTime startTime = GetDateTime();
+                DateTime endTime = GetDateTime();
+                AddShift shift = new() { StartTime = startTime, EndTime = endTime };
+                //Add record here.
+                await PostShiftAsync(shift);
+                Console.WriteLine("Record added successfully.");
+                continue;
+
+            }
+            else if (operation.Equals("v"))
+            {
+                List<ShiftRecord> shiftRecords = await GetShiftsAsync();
+                if (shiftRecords.Count > 0)
+                {
+                    Console.WriteLine("Id\tStartTime\t\tEndTime\t\t\tDuration\n");
+                    shiftRecords.ForEach(e => Console.WriteLine($"{e.Id}\t{e.StartTime:dd-mm-yyyy HH:mm:ss}\t{e.EndTime:dd-mm-yyyy HH:mm:ss}\t{e.Duration}"));
+                }
+                else
+                {
+                    Console.WriteLine("No records returned.");
+                }
+                Console.WriteLine("Press any key to restart.");
+                Console.ReadKey();
+                continue;
+
+            }
+            else
+            {
+                //Invalid operation
+                //Press any key to return to the main menu
+                //Return to main
+                Console.WriteLine("\nInvalid operation entered.\nPress any key to restart.");
+                Console.ReadKey();
+                continue;
+            }
+
+        }
+        client.Dispose();
+
+    }
+
+    public string MainMenu()
+    {
+        Console.WriteLine("Welcome to the shifts logger.");
+        Console.WriteLine("\nPick the letter that corresponds to your desired operation.");
+        string choice = getTypes.GetString("(C)reate a shift record.\n(V)iew existing shift records.\n(E)xit the application\n");
+        return choice;
+    }
+    public async Task<List<ShiftRecord>> GetShiftsAsync()
+    {
+        try
+        {
+            Stream jsonStream = await client.GetStreamAsync(client.BaseAddress);
+            List<ShiftRecord>? shifts = await JsonSerializer.DeserializeAsync<List<ShiftRecord>>(jsonStream);
+            return shifts ?? Enumerable.Empty<ShiftRecord>().ToList();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There was an error with the request");
+            Console.WriteLine(e.Message);
+            return Enumerable.Empty<ShiftRecord>().ToList();
+        }
+    }
+
+    public async Task<ShiftRecord> PostShiftAsync(AddShift shift)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime GetDateTime()
+    {
+        DateTime dateTime;
+        int year, month, day;
+
+        do
+        {
+            year = getTypes.GetInt("Enter a valid year (YYYY)");
+        }
+        while (year < DateTime.MinValue.Year || year > DateTime.MaxValue.Year || year > DateTime.Now.Year);
+
+        do
+        {
+            month = getTypes.GetInt("Enter a valid month (MM)");
+        }
+        while (month < 1 || month > 12);
+
+        int maxDays = DateTime.DaysInMonth(year, month);
+        do
+        {
+            day = getTypes.GetInt("Enter a valid day (DD)");
+        }
+        while (day < 1 || day > maxDays);
+
+
+        Console.WriteLine("Date: " + new DateTime(year, month, day).ToString("dd-MM-yyyy"));
+
+        int hour, minutes, seconds;
+        do
+        {
+            hour = getTypes.GetInt("Enter the hour of the day (24-hour time)");
+        } while (hour < 0 || hour > 23);
+
+        do
+        {
+            minutes = getTypes.GetInt("Enter the minutes(0-59)");
+        } while (minutes < 0 || minutes > 59);
+        do
+        {
+            seconds = getTypes.GetInt("Enter the seconds(0-59)");
+        } while (seconds < 0 || seconds > 59);
+
+        Console.WriteLine("Time: " + new TimeOnly(hour, minutes, seconds));
+        dateTime = new DateTime(year, month, day, hour, minutes, seconds);
+        return dateTime;
     }
 }
